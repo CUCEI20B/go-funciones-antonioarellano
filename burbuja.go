999c5b085af36433f7b5fb8ac78f58c4ec882999
Antonio Arellano <jose.arellano6698@alumnos.udg.mx>
2020-10-21 12:33:29 -0500

    Funcion de ordenamiento con burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,22 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func Burbuja(s []int64) {
+	iteracion := 0
+	permutacion := true
+	var actual int64
+	for permutacion {
+		permutacion = false
+		iteracion++
+		for actual = 0; actual < int64(len(s)-1); actual++ {
+			if s[actual] > s[actual+1] {
+				permutacion = true
+				temp := s[actual]
+				s[actual] = s[actual+1]
+				s[actual+1] = temp
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
 }
